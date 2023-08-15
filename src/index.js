29e17c20ef281e0f8613fad870a215fefe82a8c4
Bogdan Logachev <logachyovbogdan@gmail.com>
2023-08-15 13:43:55 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
 
-// You should implement your task here.
+module.exports = function towelSort(matrix) {
+    const newArr = [];
+    if (matrix === undefined || matrix.length === 0) {
+        return newArr;
+    } else {
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+        matrix.forEach((item, index) => {
+            if (index % 2 == 1) {
+                newArr.push(item.reverse());
+            } else {
+                newArr.push(item);
+            }
+        });
+        return newArr.flat();
+    }
+};
+
+// const arr = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+// ];
+
+// function spreadMyArr(someArr) {
+//     if (someArr.length === 0) {
+//         return someArr;
+//     } else {
+//         const newArr = [];
+//         someArr.forEach((item, index) => {
+//             if (index % 2 === 1) {
+//                 newArr.push(item.reverse());
+//             } else {
+//                 newArr.push(item);
+//             }
+//         });
+//         return newArr.flat();
+//     }
+// }
+
+// console.log(spreadMyArr(arr));
